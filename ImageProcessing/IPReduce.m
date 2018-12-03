b582c86abed4d1840f0fd05d6bc8811d822ba875
S3836452 <S3836452@XAP021.workspace.rug.nl>
2018-12-03 02:51:43 +0100

    new comment

--- ImageProcessing/IPReduce.m
+++ ImageProcessing/IPReduce.m
@@ -2,17 +2,15 @@ function reducedImage = IPReduce(imname,numLevels)
     clc;                                  % clear the command window
     close all;                            % close open figure windows      
     if (~ismember(numLevels,[256 128 64 32 16 8 4 2]))
-        reducedImage = 0;
+        reducedImage = 0; %not a correct level, exit with error
     else
         inputfile = [imname,'.tif'];
         figure;
         f = imread(inputfile);
         imshow(f);
-        newLevels = (1:round(256/numLevels):256) - 1;
-        newLevels(end) = 255;
-        %Agafar figura i separar els valors entre 2^numLevels
-        %agafar els valors de la imatge i enviar-los a el mes proper
-        for row = 1:length(f)
+        newLevels = (1:round(256/(numLevels-1)):257) - 1 %new space of values
+        newLevels(end) = 255; %adjust black
+        for row = 1:length(f) %project each value to the closest of the new space
             for i = 1:length(f(row,:))
                 [value,index] = min(abs(newLevels-double(f(row,i))));
                 f(row,i) = newLevels(index);
@@ -20,6 +18,6 @@ function reducedImage = IPReduce(imname,numLevels)
         end
         figure;
         imshow(f);
-        reducedImage = f;
+        reducedImage = f; %return transformed image
     end
 end
