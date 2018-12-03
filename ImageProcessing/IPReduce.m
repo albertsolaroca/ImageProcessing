47a3bbe1011de10a255d74ffc7e3c5790c91d8ba
S3836452 <S3836452@XAP021.workspace.rug.nl>
2018-12-03 01:38:08 +0100

    ex1 redone

--- ImageProcessing/IPReduce.m
+++ ImageProcessing/IPReduce.m
@@ -1,13 +1,15 @@
 function reducedImage = IPReduce(imname,numLevels)
     clc;                                  % clear the command window
     close all;                            % close open figure windows      
-    if (numLevels > 8 || numLevels < 0)
+    if (~ismember(numLevels,[256 128 64 32 16 8 4 2]))
+        reducedImage = 0;
     else
         inputfile = [imname,'.tif'];
         figure;
         f = imread(inputfile);
         imshow(f);
-        newLevels = 0:2^numLevels:256
+        newLevels = (1:round(256/numLevels):256) - 1;
+        newLevels(end) = 255;
         %Agafar figura i separar els valors entre 2^numLevels
         %agafar els valors de la imatge i enviar-los a el mes proper
         for row = 1:length(f)
