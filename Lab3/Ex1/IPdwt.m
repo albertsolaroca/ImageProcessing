674a35ecadcf51a277c1ba5290054cdec4dce6af
s3836452 <s3836452@WKS030551.workspace.rug.nl>
2018-12-10 12:46:23 +0100

    part a of 1 done

---
+++ Lab3/Ex1/IPdwt.m
@@ -0,0 +1,15 @@
+function  y = IPdwt(x,jScale)
+%IPdwt discrete wavelet transform based of Haar wavelet
+%   Detailed explanation goes here
+d = zeros(1,size(x,2)-size(x,2)/2^jScale);
+index = 0;
+for i = 1:jScale
+    c = (x(1:2:end) + x(2:2:end))/(2^(1/2));
+    inter = (x(1:2:end) - x(2:2:end))/(2^(1/2));
+    d(end-index-size(inter,2)+1:end-index) = inter;
+    index = index + size(inter,2);
+    x = c;
+end
+y = [x d];
+end
+
