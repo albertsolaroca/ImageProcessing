81fd04d254e39a536b073131343340cf8aafb0b2
S3836452 <S3836452@XAP021.workspace.rug.nl>
2018-12-15 12:39:16 +0100

    cleaned contrastsketch

--- Lab1/IPcontrastsketch.m
+++ Lab1/IPcontrastsketch.m
@@ -1,30 +1,8 @@
 function newImage = IPcontrastsketch(imname)
-
-% clc;                                  % clear the command window
-% close all;                            % close open figure windows      
-% inputfile = [imname,'.tif'];
-f = imname;
-% figure;
-% imhist(f);
-% title('Histogram of the original image');
-% ylabel('Number of pixels');
-% 
-% figure;
-% imshow(f);
-% f= double(f);
-
-minf = min(f(:));
-maxf = max(f(:));
-
-f = 255/(maxf-minf)*(f-minf);
-newImage = f;
-% figure;
-% imhist(f);
-% title('Histogram after applying contrast sketching to the image');
-% ylabel('Number of pixels');
-
-% figure;
-% imshow(f);
-
-
+%imnameunction that spread the grey values along all the domain
+ %imname has to be a double to operate correctly
+minimname = min(imname(:));
+maximname = max(imname(:));
+imname = 255/(maximname-minimname)*(imname-minimname);
+newImage = imname;
 end
