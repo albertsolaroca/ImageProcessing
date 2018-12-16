8b642ebeda865bf4067a107baf9efb4e189361b0
S3836452 <S3836452@XAP022.workspace.rug.nl>
2018-12-16 22:43:13 +0100

    damned image

--- Lab3/Ex2/script_dwt.m
+++ Lab3/Ex2/script_dwt.m
@@ -10,3 +10,7 @@ g = double(f);
 i = IPdwt2(g,2);
 j = IPdwt2scale(i,2);
 imshow(uint8(j))
+numbers_pos = [121 3; 121 130; 252 3; 252 130; 252 257; 505 3; 505 257];
+for i = 1:7
+    text(numbers_pos(i,2),numbers_pos(i,1),num2str(i));
+end
