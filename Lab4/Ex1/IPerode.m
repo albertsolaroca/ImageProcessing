9227e17d702143673140d913dd640b688088e642
S3834549 <S3834549@XAP026.workspace.rug.nl>
2018-12-24 14:28:27 +0100

    Fixed error I had by not taking into account the background

--- Lab4/Ex1/IPerode.m
+++ Lab4/Ex1/IPerode.m
@@ -1,18 +1,30 @@
 function eroded_image = IPerode(image,element)
 
 reflected_element = rot90(element,2); %Reflect the element around its origin
+A = image;                   %Convert to double
+
+A_expanded = zeros(size(A,1)+2,size(A,2)+2); %Expand image
+A_expanded(2:end-1,2:end-1) = A;            %Add image to center
+A_expanded(2:end-1,1)=0;               %Copy edges to sides
+A_expanded(2:end-1,end) = 0;
+A_expanded(1,2:end-1) = 0;
+A_expanded(end,2:end-1) = 0;
+
 %We now create 9 copies of the original image so that we will later apply
 %the reflected_element, cycling through the sides
-image_up = [image(2:end,:); image(1,:)]; 
-image_down = [image(end,:); image(1:end-1,:)];  
-image_left = [image(:,2:end) image(:,1)]; 
-image_right = [image(:,end) image(:,1:end-1)];
+A_expanded_up = [A_expanded(2:end,:); A_expanded(1,:)]; 
+A_expanded_down = [A_expanded(end,:); A_expanded(1:end-1,:)];  
+A_expanded_left = [A_expanded(:,2:end) A_expanded(:,1)]; 
+A_expanded_right = [A_expanded(:,end) A_expanded(:,1:end-1)];
 
-image_up_left = [image_up(:,2:end) image_up(:,1)];
-image_down_left = [image_down(:,2:end) image_down(:,1)];
-image_up_right = [image_up(:,end) image_up(:,1:end-1)];
-image_down_right = [image_down(:,end) image_down(:,1:end-1)];
+A_expanded_up_left = [A_expanded_up(:,2:end) A_expanded_up(:,1)];
+A_expanded_down_left = [A_expanded_down(:,2:end) A_expanded_down(:,1)];
+A_expanded_up_right = [A_expanded_up(:,end) A_expanded_up(:,1:end-1)];
+A_expanded_down_right = [A_expanded_down(:,end) A_expanded_down(:,1:end-1)];
 
-image_result = reflected_element(1,1)*image_up_left + reflected_element(1,2)*image_up + reflected_element(1,3)*image_up_right + reflected_element(2,1)*image_left + reflected_element(2,2)*image + reflected_element(2,3)*image_right + reflected_element(3,1)*image_down_left + reflected_element(3,2)*image_down + reflected_element(3,3)*image_down_right;
-eroded_image = (image_result == sum(reflected_element(:)));
+%Apply reflected_element
+A_expanded_result = reflected_element(1,1)*A_expanded_up_left + reflected_element(1,2)*A_expanded_up + reflected_element(1,3)*A_expanded_up_right + reflected_element(2,1)*A_expanded_left + reflected_element(2,2)*A_expanded + reflected_element(2,3)*A_expanded_right + reflected_element(3,1)*A_expanded_down_left + reflected_element(3,2)*A_expanded_down + reflected_element(3,3)*A_expanded_down_right;
+%Use only center of the image
+A_result = A_expanded_result(2:end-2,2:end-1);
+eroded_image = (A_result == sum(reflected_element(:)));
 end
