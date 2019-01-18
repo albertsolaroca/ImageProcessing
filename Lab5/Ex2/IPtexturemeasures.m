c5b5e425d12d76ac548747320b876a9794408ec3
S3836452 <S3836452@XAP030.workspace.rug.nl>
2019-01-18 19:03:05 +0100

    ex2 done

--- Lab5/Ex2/IPtexturemeasures.m
+++ Lab5/Ex2/IPtexturemeasures.m
@@ -1,4 +1,4 @@
-function [m,var,R,thirdMom,unif,entr] = IPtexturemeasures(image)
+function [m,std,R,thirdMom,unif,entr] = IPtexturemeasures(image)
 %IPTEXTUREMEASURES Summary of an image by different statistical measures
 %about the texture
 %   image must be a double matrix
@@ -7,9 +7,11 @@ values = unique(image); %different values
 counts = histcounts(image,values); %number of repetition
 counts = [counts sum(image == values(end),'all')]; %split the last bin
 counts(1,end-1) = counts(1,end-1) - counts(1,end);
-m = counts*values/(size(image,1)*size(image,2)); %mean
-var = counts*((values-m).^2)/(size(image,1)*size(image,2)); %variance
-norm = (size(values,1) - 1)^2; %denom to normalize
+counts = counts/sum(counts); %probability
+m = counts*values; %mean
+var = counts*((values-m).^2); %variance
+std = sqrt(var); %std
+norm = (256 - 1)^2; %denom to normalize
 R = 1 - (1/(1 + (var / norm ))); %R
 thirdMom = counts*((values-m).^3)/norm; %third moment
 unif = sum(counts.^2); %uniformity
--- Lab5/Ex2/script_texturemeasures.m
+++ Lab5/Ex2/script_texturemeasures.m
@@ -1,8 +1,18 @@
 %script to measure texture of different images
 close all;
 clc;
-f = imread('cktboard.tif');
+f = imread('bubbles.tif');
+g = imread('cktboard.tif');
+h = imread('cereal.tif');
 figure;
-imshow(f(end-99:end,end-99:end));
+subplot(1,3,1);imshow(f(end-99:end,end-99:end)); title('Bubbles');
+subplot(1,3,2);imshow(g(end-99:end,end-99:end)); title('Cktboard');
+subplot(1,3,3);imshow(h(end-99:end,end-99:end)); title('Cereal');
+
 f = double(f(end-99:end,end-99:end));
-IPtexturemeasures(f);
+g = double(g(end-99:end,end-99:end));
+h = double(h(end-99:end,end-99:end));
+
+[m1,std1,R1,thirdMom1,unif1,entr1] = IPtexturemeasures(f)
+[m2,std2,R2,thirdMom2,unif2,entr2] = IPtexturemeasures(g)
+[m3,std3,R3,thirdMom3,unif3,entr3] = IPtexturemeasures(h)
